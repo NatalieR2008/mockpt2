d2162de1162fda27826ea7c4134e63ae5b4ca955
Xariposa <90054551+509Xc0der@users.noreply.github.com>
2024-01-22 15:49:49 -0600

    Restore '1/22/2024, 3:12:16 PM Restore '1/22/2024, 8:57:38 AM Deleted blocks''

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ namespace SpriteKind {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level1`)
     player1.setPosition(120, 27)
+    chestlist = []
     for (let value of tiles.getTilesByType(sprites.dungeon.darkGroundNorthEast1)) {
         Chest = sprites.create(img`
             . . b b b b b b b b b b b b . . 
@@ -25,6 +26,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, l
             . b b . . . . . . . . . . b b . 
             `, SpriteKind.chest_)
         Chest.setPosition(value.x, value.y)
+        chestlist.push(Chest)
     }
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -86,6 +88,28 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     true
     )
 })
+function Items (Chest: Sprite) {
+    if (Math.percentChance(100)) {
+        keys = sprites.create(img`
+            . . . . . 5 . . . . . . . . . . 
+            . 5 5 . . . 5 . . . . 5 5 . . . 
+            . . 5 5 . . 5 5 . . 5 5 . . . . 
+            . . . 5 . . . . . . . . . . . . 
+            . . . . . 5 5 5 5 5 . . . . . . 
+            . . . . . 5 . 5 . 5 . 5 5 5 5 . 
+            5 5 5 . . 5 5 . 5 5 . . . . . . 
+            . . . . . 5 . 5 . 5 . . . . . . 
+            . . . . . 5 5 5 5 5 . 5 . . . . 
+            . . 5 5 . . . . 5 5 . 5 5 . . . 
+            . 5 5 . . . . . 5 5 . . 5 5 . . 
+            . 5 . . . . . . 5 5 . . . 5 . . 
+            . . . . . . . 5 5 5 . . . 5 . . 
+            . . . . . . . . 5 5 . . . . . . 
+            . . . . . . . 5 5 5 . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.powerup)
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`level5`)
     tiles.placeOnTile(player1, tiles.getTileLocation(2, 4))
@@ -96,24 +120,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, l
     tiles.placeOnTile(player1, tiles.getTileLocation(2, 1))
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.chest_, function (sprite, otherSprite) {
-    keys = sprites.create(img`
-        . . . . . 5 . . . . . . . . . . 
-        . 5 5 . . . 5 . . . . 5 5 . . . 
-        . . 5 5 . . 5 5 . . 5 5 . . . . 
-        . . . 5 . . . . . . . . . . . . 
-        . . . . . 5 5 5 5 5 . . . . . . 
-        . . . . . 5 . 5 . 5 . 5 5 5 5 . 
-        5 5 5 . . 5 5 . 5 5 . . . . . . 
-        . . . . . 5 . 5 . 5 . . . . . . 
-        . . . . . 5 5 5 5 5 . 5 . . . . 
-        . . 5 5 . . . . 5 5 . 5 5 . . . 
-        . 5 5 . . . . . 5 5 . . 5 5 . . 
-        . 5 . . . . . . 5 5 . . . 5 . . 
-        . . . . . . . 5 5 5 . . . 5 . . 
-        . . . . . . . . 5 5 . . . . . . 
-        . . . . . . . 5 5 5 . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.powerup)
     otherSprite.setImage(img`
         . b b b b b b b b b b b b b b . 
         b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
@@ -132,6 +138,9 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.chest_, function (sprite, otherS
         b b b b b b b b b b b b b b b b 
         . b b . . . . . . . . . . b b . 
         `)
+    Items(Chest)
+    keys.x = otherSprite.x
+    keys.y = otherSprite.y
 })
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
@@ -193,6 +202,24 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    Chest.setImage(img`
+        . b b b b b b b b b b b b b b . 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+        b b b b b b b d d b b b b b b b 
+        . b b b b b b c c b b b b b b . 
+        b c c c c c b c c b c c c c c b 
+        b c c c c c c b b c c c c c c b 
+        b c c c c c c c c c c c c c c b 
+        b c c c c c c c c c c c c c c b 
+        b b b b b b b b b b b b b b b b 
+        b e e e e e e e e e e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b c e e e e e e e e e e e e c b 
+        b b b b b b b b b b b b b b b b 
+        . b b . . . . . . . . . . b b . 
+        `)
     pickax = sprites.create(img`
         . . . . . . . . . 5 . . . . . . 
         . . . . 5 . . . . 5 . . . 5 . . 
@@ -211,10 +238,10 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sp
         . . . . . . . . . 5 . . . . . . 
         . . . . . . . . . 5 . . . . . . 
         `, SpriteKind.powerup)
-    tiles.setTileAt(location, sprites.dungeon.chestOpen)
-    tiles.placeOnTile(pickax, tiles.getTileLocation(13, 11))
+    pickax.x = Chest.x
+    pickax.y = Chest.y
     while (!(controller.A.isPressed())) {
-        game.showLongText("You found a pickaxe! (Can break cracked brick tiles)", DialogLayout.Bottom)
+        game.showLongText("You found a pickax! (Can break cracked brick tiles)", DialogLayout.Bottom)
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -339,85 +366,14 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, l
     tiles.setCurrentTilemap(tilemap`level3`)
     tiles.placeOnTile(player1, tiles.getTileLocation(8, 14))
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.powerup, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+})
 let pickax: Sprite = null
 let keys: Sprite = null
-let player1: Sprite = null
 let Chest: Sprite = null
-tiles.setCurrentTilemap(tilemap`level1`)
-let list = [
-sprites.create(img`
-    . . . . . 5 . . . . . . . . . . 
-    . 5 5 . . . 5 . . . . 5 5 . . . 
-    . . 5 5 . . 5 5 . . 5 5 . . . . 
-    . . . 5 . . . . . . . . . . . . 
-    . . . . . 5 5 5 5 5 . . . . . . 
-    . . . . . 5 . 5 . 5 . 5 5 5 5 . 
-    5 5 5 . . 5 5 . 5 5 . . . . . . 
-    . . . . . 5 . 5 . 5 . . . . . . 
-    . . . . . 5 5 5 5 5 . 5 . . . . 
-    . . 5 5 . . . . 5 5 . 5 5 . . . 
-    . 5 5 . . . . . 5 5 . . 5 5 . . 
-    . 5 . . . . . . 5 5 . . . 5 . . 
-    . . . . . . . 5 5 5 . . . 5 . . 
-    . . . . . . . . 5 5 . . . . . . 
-    . . . . . . . 5 5 5 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.powerup),
-sprites.create(img`
-    . . . . . 3 . . . . . . . . . . 
-    . 3 3 . . . 3 . . . . 3 3 . . . 
-    . . 3 3 . . 3 3 . . 3 3 . . . . 
-    . . . 3 . . . . . . . . . . . . 
-    . . . . . 3 3 3 3 3 . . . . . . 
-    . . . . . 3 . 3 . 3 . 3 3 3 3 . 
-    3 3 3 . . 3 3 . 3 3 . . . . . . 
-    . . . . . 3 . 3 . 3 . . . . . . 
-    . . . . . 3 3 3 3 3 . 3 . . . . 
-    . . 3 3 . . . . 3 3 . 3 3 . . . 
-    . 3 3 . . . . . 3 3 . . 3 3 . . 
-    . 3 . . . . . . 3 3 . . . 3 . . 
-    . . . . . . . 3 3 3 . . . 3 . . 
-    . . . . . . . . 3 3 . . . . . . 
-    . . . . . . . 3 3 3 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.powerup),
-sprites.create(img`
-    . . . . . 6 . . . . . . . . . . 
-    . 6 6 . . . 6 . . . . 6 6 . . . 
-    . . 6 6 . . 6 6 . . 6 6 . . . . 
-    . . . 6 . . . . . . . . . . . . 
-    . . . . . 6 6 6 6 6 . . . . . . 
-    . . . . . 6 . 6 . 6 . 6 6 6 6 . 
-    6 6 6 . . 6 6 . 6 6 . . . . . . 
-    . . . . . 6 . 6 . 6 . . . . . . 
-    . . . . . 6 6 6 6 6 . 6 . . . . 
-    . . 6 6 . . . . 6 6 . 6 6 . . . 
-    . 6 6 . . . . . 6 6 . . 6 6 . . 
-    . 6 . . . . . . 6 6 . . . 6 . . 
-    . . . . . . . 6 6 6 . . . 6 . . 
-    . . . . . . . . 6 6 . . . . . . 
-    . . . . . . . 6 6 6 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.powerup),
-sprites.create(img`
-    . . . . . b . . . . . . . . . . 
-    . b b . . . b . . . . b b . . . 
-    . . b b . . b b . . b b . . . . 
-    . . . b . . . . . . . . . . . . 
-    . . . . . b b b b b . . . . . . 
-    . . . . . b . b . b . b b b b . 
-    b b b . . b b . b b . . . . . . 
-    . . . . . b . b . b . . . . . . 
-    . . . . . b b b b b . b . . . . 
-    . . b b . . . . b b . b b . . . 
-    . b b . . . . . b b . . b b . . 
-    . b . . . . . . b b . . . b . . 
-    . . . . . . . b b b . . . b . . 
-    . . . . . . . . b b . . . . . . 
-    . . . . . . . b b b . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-]
+let chestlist: Sprite[] = []
+let player1: Sprite = null
 let mySprite = game.askForString("Choose Difficulty (E,M,H)", 1)
 if (mySprite == "E") {
     info.startCountdown(600)
@@ -425,27 +381,6 @@ if (mySprite == "E") {
     info.startCountdown(480)
 } else {
     info.startCountdown(300)
-}
-for (let value of tiles.getTilesByType(sprites.dungeon.darkGroundNorthEast1)) {
-    Chest = sprites.create(img`
-        . . b b b b b b b b b b b b . . 
-        . b e 4 4 4 4 4 4 4 4 4 4 e b . 
-        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
-        b e e e e e e e e e e e e e e b 
-        b e e e e e e e e e e e e e e b 
-        b b b b b b b d d b b b b b b b 
-        c b b b b b b c c b b b b b b c 
-        c c c c c c b c c b c c c c c c 
-        b e e e e e c b b c e e e e e b 
-        b e e e e e e e e e e e e e e b 
-        b c e e e e e e e e e e e e c b 
-        b b b b b b b b b b b b b b b b 
-        . b b . . . . . . . . . . b b . 
-        `, SpriteKind.chest_)
-    Chest.setPosition(value.x, value.y)
 }
 player1 = sprites.create(img`
     . f f f . f f f f . f f f . 
@@ -468,3 +403,27 @@ player1 = sprites.create(img`
 player1.setPosition(120, 27)
 controller.moveSprite(player1)
 scene.cameraFollowSprite(player1)
+tiles.setCurrentTilemap(tilemap`level1`)
+chestlist = []
+for (let value of tiles.getTilesByType(sprites.dungeon.darkGroundNorthEast1)) {
+    Chest = sprites.create(img`
+        . . b b b b b b b b b b b b . . 
+        . b e 4 4 4 4 4 4 4 4 4 4 e b . 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+        b e e e e e e e e e e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b b b b b b b d d b b b b b b b 
+        c b b b b b b c c b b b b b b c 
+        c c c c c c b c c b c c c c c c 
+        b e e e e e c b b c e e e e e b 
+        b e e e e e e e e e e e e e e b 
+        b c e e e e e e e e e e e e c b 
+        b b b b b b b b b b b b b b b b 
+        . b b . . . . . . . . . . b b . 
+        `, SpriteKind.chest_)
+    Chest.setPosition(value.x, value.y)
+    chestlist.push(Chest)
+}
